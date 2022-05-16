64c85c1196d43ac76000e5191d18ceda3386e5af
Bruce <1016509070@qq.com>
2022-05-16 09:51:19 +0800

    no message

---
+++ 狗店咖啡/SQL/select.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Add Customer</title>
+
+	<link rel="stylesheet" type="text/css" href="../CSS/select.css">
+</head>
+<body>
+	<h2> Register Order Information</h2>
+
+	<form name="form1" action="../addstudent_manager.php" method="POST">
+		<div class="container">
+			<div class="form_group">
+				<label>User:</label>
+				<input type="text" name="user" value="" required>
+			</div>
+			<div class="form_group">
+				<label>Password:</label>
+				<input type="text" name="password" value="0000" required>
+			</div>
+			<div class="form_group">
+				<label>Orders:</label>
+				<input type="text" name="orders" value="Latte" required>
+			</div>
+			<div class="form_group">
+				<label>Note:</label>
+				<input type="text" name="note" value="empty" required>
+			</div>
+			<div class="form_group">
+				<input id="submit" type="submit" name="submit" value="Submit" required>
+			</div>
+		</div>
+
+
+	</form>
+
+
+</body>
+</html>
---
+++ 狗店咖啡/addstudent_manager.php
@@ -0,0 +1,32 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
+    echo 'good';
+    $conn = mysqli_connect('localhost','root','','socialdog') or die("Connect Failed: " . mysqli_connect_error());
+
+    if(isset($_POST['user'])&&isset($_POST['password'])&&isset($_POST['orders'])&&isset($_POST['note']))
+    {
+        $user = $_POST['user'];
+        $password = $_POST['password'];
+        $note = $_POST['note'];
+        $orders = $_POST['orders'];
+
+        $sql = "INSERT INTO `order` (`user`,`password`,`orders`,`note`) VALUES ('$user','$password','$orders','$note');";
+        $query= mysqli_query($conn,$sql);
+        if($query){
+            echo "<script>window.alert('Add Student Success');</script>";
+            echo "<script>window.location.href='ManagementPage.php';</script>";
+        }else{
+            echo "<script>window.alert('Query Error !!!!');</script>";
+            echo "<script>window.location.href='ManagementPage.php';</script>";
+        }
+
+    }else{
+        echo 'Bad';
+    }
+}
+else
+{
+    echo "something bad happens";
+}
+
+?>
